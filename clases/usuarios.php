c56c963611a24edca62aa8bab020f74c00f23225
RoBa21zZ <robertomlug@gmail.com>
2025-11-04 21:44:18 +0100

    cosas con edad

--- clases/usuarios.php
+++ clases/usuarios.php
@@ -1,17 +1,48 @@
 <?php
 include "conf/conexion.php";
 
-class Usuarios extends DatabaseConnection {
+class Usuarios {
+    private $conn;
+    private $id;
+    private $nombre;
+    private $fecha_nacimiento;
+
+    public function __construct($db) {
+       $this->conn = $db;
+    }
     
-    public function obtenerUsuarios($id){
-        $sql = "SELECT * FROM usuario WHERE id = ?";
-        $stmt = $this->conectar()->prepare($sql);
-        $stmt->execute([$id]);
+    public function obtenerUsuarios(){
+        $sql = "SELECT * FROM usuario";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute();
         $usuarios = $stmt->fetchAll();
 
-        foreach($usuarios as $usuario){
-            echo "<br>Usuario: " . $usuario['nombre'] . " " ;
-        }
+        return $usuarios;
+    }
+
+    public function obtenerFechaDeNacimiento($id){
+        $sql = "Select fechaDeNacimiento from usuario where id = ?";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute([$id]);
+        $resultado = $stmt->fetchAll();
+
+        return $resultado[0]['fechaDeNacimiento'];
+    }
+
+    public function introducirUsuario($nombre, $fecha_nacimiento){
+        $sql = "INSERT INTO usuario (nombre, fechaDeNacimiento) VALUES (?, ?)";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute([$nombre, $fecha_nacimiento]);
+
+    }
+
+    public function mostrarEdad($fecha){
+        
+        $fechaDate = new DateTime($fecha);
+        $hoy = new DateTime();
+        $annos = $hoy->diff($fechaDate)->y;
+
+        return $annos . "<br>";
     }
     
 }
--- index.php
+++ index.php
@@ -1,9 +1,30 @@
 <?php
-    
-    
-    require_once "clases/usuarios.php";
-    
-    
+
+use function PHPSTORM_META\type;
+
+    require_once "clases/usuarios.php";    
+
+    $database = new DatabaseConnection();
+    $db = $database->conectar();
+
+    //$mensaje = "";
+    $usuariosClase = new Usuarios($db);
+
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        $nombre = $_POST["nombre"];
+        $fecha_nacimiento = $_POST["fecha_nacimiento"];
+
+        if(!empty($nombre) && !empty($fecha_nacimiento)){
+            if($usuariosClase->introducirUsuario($nombre, $fecha_nacimiento)){
+                session_start();
+                $_SESSION['mensaje'] = "Usuario introducido correctamente";
+            } else {
+                $_SESSION['mensaje'] = "Error al introducir el usuario";
+            }
+        }
+        header("Location: " . $_SERVER['PHP_SELF']);
+        exit;
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -12,6 +33,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <style>
+        .div1{
+            border: 1px solid black;
+            width: 300px;
+            height: auto;
+            padding: 10px;
+            margin: 10px;
+        }
+
+        form{
+            border: 1px solid black;
+            padding: 0px;
+            margin: 0px;
+            
+        }
+        
+        .botonEnviar {
+            background-color: blue;
+            color: white;
+            padding: 10px;
+            border-radius: 5px;
+            border: none;
+            display: flex;
+            
+            
+        }
+    </style>
 </head>
 
 <body>
@@ -26,17 +75,49 @@
 
     echo date("D-M-Y H:i:s");
 
-    $db = new Usuarios();
-    $db->obtenerUsuarios(3);
+    
+    $usuariosClase->obtenerUsuarios(4);
 
+    
+    $fechaUsuario4 = $usuariosClase->obtenerFechaDeNacimiento(4);
+
+    $fechaTransformada = date("d-M-Y",strtotime($fechaUsuario4));
+
+    echo "<br>";
+
+    echo "Tipo que viene de la base de datos " . gettype($fechaUsuario4);
+    echo "<br> Tipo transformado " . gettype($fechaTransformada) . " " . $fechaTransformada;
     // print_r(PDO::getAvailableDrivers());
 
     /* $usuairos = new Usuarios();
     
     print_r($usuairos->obtenerUsuarios()); */
 
-    //$usuarios = $usuario->obtenerUsuarios();
+    $usuarios = $usuariosClase->obtenerUsuarios();
     
+    foreach($usuarios as $usuario){
+        echo "Nombre " . $usuario["nombre"] . " Edad " . $usuariosClase->mostrarEdad($usuario["fechaDeNacimiento"]);
+    }
+    ?>
+
+    <h3>Formulario de prueba para ver tema  fehcas y cosas</h3>
+
+    <div class="div1">
+        <form action="" method="post">
+            <label for="nombre">Nombre:</label>
+            <input type="text" name="nombre" id="nombre" required>
+            
+            <br><br>
+            
+            <label for="fecha_nacimiento">Fecha de Nacimiento:</label>  
+            <input type="date" name="fecha_nacimiento" id="fecha_nacimiento" required>
+            
+            <br><br>
+            
+            <input type="submit" value="Introducir usuairo" class="botonEnviar">
+        </form>
+    </div>
+    <?php
     
     ?>
 </body>
